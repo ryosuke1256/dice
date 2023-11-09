ab4bcd37af94f980b058a3665f16cf103466864a
Ryosuke <engineerxx1256@gmail.com>
2023-11-09 12:27:00 +0900

    Create Dice.cs

---
+++ Dice.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace source {
+  class Program {
+    public static void Main(string[] args) {
+      Dice dice = new Dice();
+      int diceNumber = dice.Throw();
+      Console.WriteLine($"dice number is {diceNumber}");
+    }
+  }
+
+  class Dice {
+    public int Throw() {
+      Random rand = new Random();
+      return rand.Next(1, 7);
+    }
+  }
+}
